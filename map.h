7a14ad25f6077264722637e9bd38d64964d57569
cznull <32773496+cznull@users.noreply.github.com>
2018-02-27 22:49:09 +0800

    Add files via upload

---
+++ map.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <gl/glew.h>
+#include <math.h>
+#include <stdio.h>
+
+int cudainit(HWND hWnd);
+int cudacalc(void);
