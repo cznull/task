e3015e4d9cfda29e8a7751d804d53de3640b5ee0
cznull <32773496+cznull@users.noreply.github.com>
2018-02-27 22:34:07 +0800

    Add files via upload

---
+++ mpa.cpp
@@ -0,0 +1,176 @@
+#include "stdafx.h"
+#include "resource.h"
+
+#define MAX_LOADSTRING 100
+
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
+
+HDC hdc1, hdcc, hdc2;
+int cx, cy;
+int startt = 0;
+GLuint vbo;
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int, HWND*);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+	MSG msg;
+	HWND hWnd;
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDS_WIN32PROJECT1, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+	if (!InitInstance(hInstance, nCmdShow, &hWnd))
+	{
+		return FALSE;
+	}
+st:
+	if (startt) {
+		for (;;) {
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			{
+				if (msg.message == WM_QUIT)
+					break;
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else if (startt) {
+			}
+			else {
+				goto st;
+			}
+		}
+	}
+	else {
+		while (GetMessage(&msg, nullptr, 0, 0))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg); if (startt) {
+				goto st;
+			}
+		}
+	}
+	return 0;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	return RegisterClassEx(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, HWND* hWnd)
+{
+	hInst = hInstance; // 将实例句柄存储在全局变量中
+
+	*hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, 0, 0, 1060, 1040, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(*hWnd, nCmdShow);
+	UpdateWindow(*hWnd);
+
+	return TRUE;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		switch (wmId) {
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	break;
+	}
+	case WM_PAINT: {
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	case WM_CREATE: {
+		SetTimer(hWnd, 1, 10, NULL);
+		hdc1 = GetDC(hWnd);
+		hdc2 = GetDC(NULL);
+		PIXELFORMATDESCRIPTOR pfd = {
+			sizeof(PIXELFORMATDESCRIPTOR),
+			1,
+			PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER | PFD_STEREO,
+			PFD_TYPE_RGBA,
+			24,
+			0,0,0,0,0,0,0,0,
+			0,
+			0,0,0,0,
+			32,
+			0,0,
+			PFD_MAIN_PLANE,
+			0,0,0,0
+		};
+		int uds = ::ChoosePixelFormat(hdc1, &pfd);
+		::SetPixelFormat(hdc1, uds, &pfd);
+		HGLRC m_hrc = ::wglCreateContext(hdc1);
+		::wglMakeCurrent(hdc1, m_hrc);
+		glewInit();
+		glDisable(GL_DEPTH_TEST);
+		glEnable(GL_TEXTURE_2D);
+		glClearColor(1.0, 1.0, 1.0, 1.0);
+
+		
+		glFrustum(-0.5, 0.5, -0.5, 0.5, 0.5, 2.0);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+
+		((bool(_stdcall*)(int))wglGetProcAddress("wglSwapIntervalEXT"))(0);
+
+		
+		break;
+	}
+	case WM_SIZE: {
+		cx = lParam & 0xffff;
+		cy = (lParam & 0xffff0000) >> 16;
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		break;
+	}
+	case WM_KEYDOWN: {
+		switch (wParam) {
+		case ' ': {
+			startt = !startt;
+			break;
+		}
+		}
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
